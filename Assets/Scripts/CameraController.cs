7fe1e04e6bc6317fac5f959dfcf9928acbd94281
LautaroCoccia <lautaroauditore@gmail.com>
2021-05-19 19:46:23 -0300

    Add CameraController.cs

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private Transform player;
+    Vector3 offset;
+    private void Start()
+    {
+        offset = transform.position;
+    }
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position =new Vector3(player.position.x, player.position.y + offset.y, offset.z);
+        transform.LookAt(player);
+    }
+}
