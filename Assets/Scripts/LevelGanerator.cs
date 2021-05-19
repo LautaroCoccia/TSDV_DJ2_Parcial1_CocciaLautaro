2c5cbd4587632b804d0f8889ab39bf1268762862
Lautaro Coccia <lautaroauditore@gmail.com>
2021-05-18 22:29:20 -0300

    Update LevelGenerator.cs

--- Assets/Scripts/LevelGanerator.cs
+++ Assets/Scripts/LevelGanerator.cs
@@ -13,10 +13,10 @@ public class LevelGanerator : MonoBehaviour
     }
     public List<Pool> pools;
     public Dictionary<string, Queue<GameObject>> poolDictionary;
+    int Position;
     // Start is called before the first frame update
     void Start()
     {
-        int Position;
         poolDictionary = new Dictionary<string, Queue<GameObject>>();
         foreach (Pool pool in pools)
         {
@@ -26,7 +26,14 @@ public class LevelGanerator : MonoBehaviour
                 Position = pool.size;
                 WallInitializer(pool.size, pool.prefab);
             }
-            
+            else if (pool.tag == "Floor")
+            {
+                FloorInitializer(Position, pool.prefab);
+            }
+            else if(pool.tag == "BrickWall")
+            {
+                BrickWallInitializer(Position, pool.prefab);
+            }
         }
     }
     void WallInitializer(int size, GameObject prefab)
@@ -45,5 +52,33 @@ public class LevelGanerator : MonoBehaviour
                 }
             }
         }
+    }
+    void FloorInitializer(int size, GameObject prefab)
+    {
+        GameObject obj = Instantiate(prefab);
+        obj.transform.localScale = new Vector3( (size / 4) + 0.5f, 1, (size / 4) + 0.5f);
+        obj.transform.position = new Vector3(size + 1, 0, size + 1);
+    }
+    void BrickWallInitializer(int size, GameObject prefab)
+    {
+        float x = 0;
+        float z = 0;
+
+        for (int i = 0; i < (size * 2) + 1; i++)
+        {
+            do
+            {
+                x = Random.Range(1, (size *2 ) + 1);
+                z = Random.Range(1, (size *2) + 1);
+            } while (x % 2 == 0 && z %2==0);
+
+            GameObject obj = Instantiate(prefab);
+            obj.transform.position = new Vector3(x, 0.5f, z);
+            
+        }
+
+        
+        
+        
     }
 }
