a6c62b8de98fc83a4f5e34dbd314a08b029642d1
LautaroCoccia <lautaroauditore@gmail.com>
2021-05-21 18:48:17 -0300

    Add Enemy

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class LevelGenerator : MonoBehaviour
 {
+    [SerializeField] int width;
+    [SerializeField] int height;
+
     [System.Serializable]
     public class Pool
     {
@@ -12,8 +15,8 @@ public class LevelGenerator : MonoBehaviour
         public int size;
     }
     public List<Pool> pools;
+    public List<Vector3> positionsList;
     public Dictionary<string, Queue<GameObject>> poolDictionary;
-    int Position;
     // Start is called before the first frame update
     void Start()
     {
@@ -23,26 +26,30 @@ public class LevelGenerator : MonoBehaviour
             Queue<GameObject> objectPool = new Queue<GameObject>();
             if(pool.tag == "Wall")
             {
-                Position = pool.size;
+                pool.size = (width * height) / 2;
                 WallInitializer(pool.size, pool.prefab);
             }
             else if (pool.tag == "Floor")
             {
-                FloorInitializer(Position, pool.prefab);
+                FloorInitializer(pool.prefab);
             }
             else if(pool.tag == "BrickWall")
             {
-                BrickWallInitializer(Position, pool.prefab);
+                BrickWallInitializer(pool.size, pool.prefab);
+            }
+            else if (pool.tag == "Enemy")
+            {
+                BrickWallInitializer(pool.size, pool.prefab);
             }
         }
     }
     void WallInitializer(int size, GameObject prefab)
     {
-        for (int i = 0; i < (size * 2) + 1; i++)
+        for (int i = 0; i < (width * 2 ) + 1; i++)
         {
             if ((i % 2) == 0 && i > 0)
             {
-                for (int j = 0; j < (size * 2) + 1; j++)
+                for (int j = 0; j < (height * 2 ) + 1; j++)
                 {
                     if ((j % 2) == 0 && j > 0)
                     {
@@ -53,30 +60,125 @@ public class LevelGenerator : MonoBehaviour
                 }
             }
         }
+        for( int i = 0; i< 4; i++)
+        {
+            GameObject obj = Instantiate(prefab);
+            obj.transform.SetParent(gameObject.transform);
+            switch(i)
+            {
+                case 0:
+                    obj.transform.position = new Vector3(0, 0.5f,height + 1);
+                    obj.transform.localScale = new Vector3(1, 1, (height * 2) + 2);
+                    break;
+                case 1:
+                    obj.transform.position = new Vector3((width * 2) +2, 0.5f, height + 1);
+                    obj.transform.localScale = new Vector3(1, 1, (height * 2) + 3) ;
+                    break;
+                case 2:
+                    obj.transform.position = new Vector3((width) + 1, 0.5f, 0);
+                    obj.transform.localScale = new Vector3((width * 2) + 3, 1, 1);
+                    break;
+                case 3:
+                    obj.transform.position = new Vector3((width) + 1, 0.5f, (height *2) +2);
+                    obj.transform.localScale = new Vector3((width * 2) + 3, 1, 1);
+                    break;
+            }
+        }
     }
-    void FloorInitializer(int size, GameObject prefab)
+    void FloorInitializer( GameObject prefab)
     {
+        float thisHeight;
+        float thisWidth;
+        if (width < 4)
+            thisWidth = 1;
+        else
+            thisWidth = width /4;
+
+        if (height < 4)
+            thisHeight = 1;
+        else
+            thisHeight = height / 4;
         GameObject obj = Instantiate(prefab);
         obj.transform.SetParent(gameObject.transform);
-        obj.transform.localScale = new Vector3( (size / 4) + 0.5f, 1, (size / 4) + 0.5f);
-        obj.transform.position = new Vector3(size + 1, 0, size + 1);
+        obj.transform.localScale = new Vector3( thisWidth , 1, thisHeight);
+        obj.transform.position = new Vector3(width + 1, 0, height + 1);
     }
-    void BrickWallInitializer(int size, GameObject prefab)
+    void BrickWallInitializer(int size,GameObject prefab)
     {
         float x = 0;
         float z = 0;
-
-        for (int i = 0; i < (size * 2) + 1; i++)
+        bool canSpawn = false;
+        if(size ==0)
+        {
+            size = (width * height) / 4;
+        }
+        for (int i = 0; i < (size) + 1; i++)
         {
             do
             {
-                x = Random.Range(2, (size *2 ) + 1);
-                z = Random.Range(2, (size *2) + 1);
-            } while (x % 2 == 0 && z %2==0);
+                x = Random.Range(2, ( width * 2 + 1));
+                z = Random.Range(2, ( height * 2 + 1));
+                if (positionsList == null && x % 2 == 0 && z % 2 == 0)
+                    positionsList.Add(new Vector3(x, 0.5f, z));
+                else
+                {
+                    for(int j = 0; j<positionsList.Count; j++ )
+                    {
+                        if (positionsList[j] == new Vector3(x, 0.5f, z))
+                            canSpawn = false;
+                        else
+                        {
+                            canSpawn = true;
+                            break;
+                        }
+                    }
+                }
+            } while (x % 2 == 0 && z %2==0 && canSpawn == true);
+            canSpawn = false;
+            positionsList.Add(new Vector3(x, 0.5f, z));
             GameObject obj = Instantiate(prefab);
             obj.transform.SetParent(gameObject.transform);
             obj.transform.position = new Vector3(x, 0.5f, z);
-            
+            Debug.Log(positionsList.Count);
+        }
+    }
+
+    void EnemyInitializer(int size ,GameObject prefab)
+    {
+        float x = 0;
+        float z = 0;
+        bool canSpawn = false;
+        if(size ==0)
+        {
+            size = (width * height) / 8;
+        }
+        for (int i = 0; i < (size) + 1; i++)
+        {
+            do
+            {
+                x = Random.Range(2, (width * 2 + 1));
+                z = Random.Range(2, (height * 2 + 1));
+                if (positionsList == null && x % 2 == 0 && z % 2 == 0)
+                    positionsList.Add(new Vector3(x, 0.5f, z));
+                else
+                {
+                    for (int j = 0; j < positionsList.Count; j++)
+                    {
+                        if (positionsList[j] == new Vector3(x, 0.5f, z))
+                            canSpawn = false;
+                        else
+                        {
+                            canSpawn = true;
+                            break;
+                        }
+                    }
+                }
+            } while (x % 2 == 0 && z % 2 == 0 && canSpawn == true);
+            canSpawn = false;
+            positionsList.Add(new Vector3(x, 0.5f, z));
+            GameObject obj = Instantiate(prefab);
+            obj.transform.SetParent(gameObject.transform);
+            obj.transform.position = new Vector3(x, 0.5f, z);
         }
     }
 }
