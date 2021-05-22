8fe95397410b4f92c96e8fdccfbcad7b4f69d0ee
LautaroCoccia <lautaroauditore@gmail.com>
2021-05-22 03:30:00 -0300

    Update BombController.cs

--- Assets/Scripts/BombController.cs
+++ Assets/Scripts/BombController.cs
@@ -6,25 +6,58 @@ public class BombController : MonoBehaviour
 {
     [SerializeField] GameObject bombPrefab;
     [SerializeField] float TimeToExplote = 2;
+    [SerializeField] float actualTime = 0;
     int bombsAlive = 0;
     GameObject bomb;
-
-    
+    Vector3 bombPos;
     // Update is called once per frame
     void Update()
     {
         if(Input.GetKeyDown(KeyCode.Space) && bombsAlive==0)
         {
             bomb = Instantiate(bombPrefab);
+            bomb.SetActive(false);
             bomb.transform.position =new Vector3( Mathf.Round( transform.position.x), transform.position.y, Mathf.Round(transform.position.z));
-            StartCoroutine(Timer());
+            bombPos = bomb.transform.position;
             bombsAlive++;
+            
+        }
+        if(bombsAlive>0)
+        {
+            if (transform.position.x < bomb.transform.position.x - 1 || transform.position.x > bomb.transform.position.x + 1 || transform.position.z < bomb.transform.position.z - 1 || transform.position.z > bomb.transform.position.z + 1)
+            {
+                bomb.SetActive(true);
+                StartCoroutine(Timer());
+            }
         }
     }
     IEnumerator Timer()
     {
         yield return new WaitForSeconds(TimeToExplote);
-        bombsAlive--;
+        
+        bombsAlive = 0;
+        actualTime = 0;
+        ExplotionHit();
         Destroy(bomb);
+    }
+    void ExplotionHit()
+    {
+        RaycastHit hit;
+        
+        if (Physics.Raycast(bombPos, transform.TransformDirection(Vector3.right), out hit, 1) ||
+            Physics.Raycast(bombPos, transform.TransformDirection(Vector3.left), out hit, 1) ||
+            Physics.Raycast(bombPos, transform.TransformDirection(Vector3.forward), out hit, 1) ||
+            Physics.Raycast(bombPos, transform.TransformDirection(Vector3.back), out hit, 1))
+        {
+            if(hit.transform.tag == "Player")
+            {
+                transform.position = new Vector3(1, 0.5f, 1);
+            }
+            else if(hit.transform.tag == "Enemy"|| hit.transform.tag == "BrickWall")
+            {
+                Destroy(hit.transform.gameObject);
+            }
+        }
+        
     }
 }
