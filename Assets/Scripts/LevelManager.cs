fb8954fff0ff706c8b6a7b18b5b55ef4433a2197
LautaroCoccia <lautaroauditore@gmail.com>
2021-05-21 22:12:45 -0300

    Add LevelManager

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    [SerializeField] int lives = 3;
+    [SerializeField] enum GameStates
+    {
+        Play,
+        Pause,
+        Win,
+        Lose
+    }
+    GameStates actualState;
+    // Start is called before the first frame update
+    void Start()
+    {
+        actualState = GameStates.Play;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
