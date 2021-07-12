24908a4c59d25adb009c9b088d987c44ca206e86
LautaroCoccia <lautaroauditore@gmail.com>
2021-07-12 15:49:58 -0300

    Add Door Controller

---
+++ Assets/Scripts/DoorController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour
+{
+    Material material;
+    // Start is called before the first frame update
+    void Start()
+    {
+        material = gameObject.GetComponent<MeshRenderer>().material;
+        material.color = new Color(material.color.r, material.color.g, material.color.b, 0.5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
