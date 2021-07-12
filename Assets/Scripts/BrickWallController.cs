35a0b904a9abecc0c8d5f562bafa4635c75ed032
LautaroCoccia <lautaroauditore@gmail.com>
2021-07-12 12:55:39 -0300

    Add BrickWall Controller

---
+++ Assets/Scripts/BrickWallController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrickWallController : MonoBehaviour, IHitable
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void OnHit()
+    {
+        Destroy(gameObject);
+    }
+}
