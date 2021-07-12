a0fdfe3b76eb44e3db9b9259452dee5aafdce49e
LautaroCoccia <lautaroauditore@gmail.com>
2021-07-12 19:25:43 -0300

    Update DoorController.cs

--- Assets/Scripts/DoorController.cs
+++ Assets/Scripts/DoorController.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DoorController : MonoBehaviour
+public class DoorController : MonoBehaviour, IHitable
 {
     Material material;
+    LevelManager levelManager;
     // Start is called before the first frame update
     void Start()
     {
+        levelManager = LevelManager.Get();
         material = gameObject.GetComponent<MeshRenderer>().material;
         material.color = new Color(material.color.r, material.color.g, material.color.b, 0.5f);
     }
@@ -15,7 +17,14 @@ public class DoorController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+    public void OnHit()
+    {
+        if(levelManager.GetIsActiveDoor())
+        {
+            Debug.Log("Ganaste");
+        }
     }
 
 }
