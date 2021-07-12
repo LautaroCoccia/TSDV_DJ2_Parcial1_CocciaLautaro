6d48cac906ba812275502b605d10c62875002c18
LautaroCoccia <lautaroauditore@gmail.com>
2021-07-12 20:25:43 -0300

    Update BombController.cs

--- Assets/Scripts/BombController.cs
+++ Assets/Scripts/BombController.cs
@@ -1,12 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using System;
 public class BombController : MonoBehaviour
 {
     [SerializeField] float timeToExplote = 2;
     [SerializeField] float range = 1;
     GameObject bomb;
+    public static Action DestroyBomb;
 
     private void Start()
     {
@@ -18,28 +19,49 @@ public class BombController : MonoBehaviour
         yield return new WaitForSeconds(timeToExplote);
         
         ExplotionHit();
+        DestroyBomb?.Invoke();
         Destroy(bomb);
     }
     void ExplotionHit()
     {
         RaycastHit hit;
         GameObject obj;
-        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.right), out hit, range)) 
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.right), out hit, range/2)) 
         {
             obj = hit.transform.gameObject;
             OnHit(obj);
         }
-        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.left), out hit, range))
+        else if(Physics.Raycast(transform.position, transform.TransformDirection(Vector3.right), out hit, range / 2))
         {
             obj = hit.transform.gameObject;
             OnHit(obj);
         }
-        if(Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, range))
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.left), out hit, range/2))
         {
             obj = hit.transform.gameObject;
             OnHit(obj);
         }
-        if(Physics.Raycast(transform.position, transform.TransformDirection(Vector3.back), out hit, range))
+        else if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.left), out hit, range))
+        {
+            obj = hit.transform.gameObject;
+            OnHit(obj);
+        }
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, range/2))
+        {
+            obj = hit.transform.gameObject;
+            OnHit(obj);
+        }
+        else if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, range))
+        {
+            obj = hit.transform.gameObject;
+            OnHit(obj);
+        }
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.back), out hit, range/2))
+        {
+            obj = hit.transform.gameObject;
+            OnHit(obj);
+        }
+        else if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.back), out hit, range))
         {
             obj = hit.transform.gameObject;
             OnHit(obj);
@@ -48,10 +70,14 @@ public class BombController : MonoBehaviour
 
     void OnHit(GameObject hit)
     {
-        if (hit.transform.tag == "Hitable" || hit.transform.tag == "Player")
+        if (hit.transform.tag == "Hitable" )
         {
             hit.transform.gameObject.GetComponent<IHitable>().OnHit();
             Destroy(hit.transform.gameObject);
+        }
+        else if ( hit.transform.tag == "Player")
+        {
+            hit.transform.gameObject.GetComponent<IHitable>().OnHit();
         }
     }
 }
