28c040fe28153f9794494e446971171fdb8cc60b
LautaroCoccia <lautaroauditore@gmail.com>
2021-07-13 00:02:28 -0300

    Add Revitalizer Item

---
+++ Assets/Scripts/Items/Revitalizer.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Revitalizer : MonoBehaviour, IHitable
+{
+    public void OnHit()
+    {
+        LevelManager.Get().OneLiveUp();
+        Destroy(gameObject);
+    }
+}
