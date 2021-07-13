0f69271e48a61778a7a6811d5090c6de1ead40d3
LautaroCoccia <lautaroauditore@gmail.com>
2021-07-12 23:04:53 -0300

    Update BrickWallController.cs

--- Assets/Scripts/BrickWallController.cs
+++ Assets/Scripts/BrickWallController.cs
@@ -21,12 +21,11 @@ public class BrickWallController : MonoBehaviour, IHitable
     {
         if(!levelManager.GetDoorExists())
         {
-            if (Random.Range(1, 100) < levelManager.GetChanceOfDoorSpawn() || levelManager.GetBrickWallLeft() <= 1)
+            if (Random.Range(1, 100) < levelManager.GetChanceOfDoorSpawn() || levelManager.GetBrickWallLeft() == 0)
             {
                 GameObject obj = Instantiate(doorPrefab);
                 obj.transform.position = transform.position;
                 levelManager.setDoorExists();
-                levelManager.setActiveDoor();
             }
         }
         
