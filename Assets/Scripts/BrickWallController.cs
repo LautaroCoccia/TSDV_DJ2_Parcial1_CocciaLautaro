283e2414ce7bde963416a7c24aba8cf5a327e972
LautaroCoccia <lautaroauditore@gmail.com>
2021-07-12 21:44:04 -0300

    Update BrickWallController.cs

--- Assets/Scripts/BrickWallController.cs
+++ Assets/Scripts/BrickWallController.cs
@@ -5,11 +5,11 @@ using UnityEngine;
 public class BrickWallController : MonoBehaviour, IHitable
 {
     [SerializeField] GameObject doorPrefab ;
-    
+    LevelManager levelManager;
     // Start is called before the first frame update
     void Start()
     {
-        
+        levelManager = LevelManager.Get();
     }
 
     // Update is called once per frame
@@ -19,7 +19,8 @@ public class BrickWallController : MonoBehaviour, IHitable
     }
     public void OnHit()
     {
-        if(Random.Range(1, 100) < LevelManager.Get().GetChanceOfDoorSpawn())
+        if()
+        if(Random.Range(1, 100) < levelManager.GetChanceOfDoorSpawn() || levelManager.GetBrickWallLeft()<=1)
         {
             GameObject obj = Instantiate(doorPrefab);
             obj.transform.position = transform.position;
